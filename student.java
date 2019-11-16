48794bf20d461f6b05d9aaff87258da326b087f0
neeleshgangopadhyay <53870966+neeleshgangopadhyay@users.noreply.github.com>
2019-11-16 12:33:15 +0530

    Add files via upload

--- student.java
+++ student.java
@@ -1,3 +1,4 @@
+ 
 package CIE;
 import java.util.*;
 public class student
@@ -8,54 +9,23 @@ public class student
   int sem;
   Scanner sc=new Scanner(System.in);
   public void getdata()
-  {
+  {  System.out.println("Enter the usn of the student:"); 
      usn=sc.next();
+     System.out.println("Enter the name of the student:"); 
      name=sc.nextLine();
+     name=sc.nextLine();
+     System.out.println("Enter the semester of the student:"); 
      sem= sc.nextInt();
    }
   public void showdata()
   {
 
-     System.out.println("the usn is :"+usn);
-     System.out.println("the name is:"+name);
-     System.out.println("the  semester is"+sem);
+     System.out.println("the usn is : "+usn);
+     System.out.println("the name is: "+name);
+     System.out.println("the  semester is "+sem);
 
    }
 
 
 }
-class Internals
-{
-
-   int marks[]=new int[5];
-   Scanner s=new Scanner(System.in);
-  public void getdata()
-   {
-
-
-     for(int i=0;i<5;i++)
-     {
-         marks[i]=s.nextInt();
-
-     }
-   }
-  public  void showdata()
-    {
-
-       for(int j=0;j<5;j++)
-       {
-          System.out.println("the subject"+(j+1)+"is:"+marks[j]);
-        }
-
-      }
-
- }
-
-
-
-
-
-
-
-
-
+ 
